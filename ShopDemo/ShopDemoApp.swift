97991c9edba5fc11d3aeda5ffe8514a589335c41
Neil A <neil@crystalstudios.net>
2021-08-08 07:05:33 -0400

    Initial Commit

---
+++ ShopDemo/ShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDemoApp.swift
+//  ShopDemo
+//
+//  Created by Neil Ajodah on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
