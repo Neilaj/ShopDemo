96cacb7fed0d61c40434bf4aa7cb45a557a6535f
Neil A <neil@crystalstudios.net>
2021-08-08 07:37:30 -0400

    Modified HomeView

---
+++ ShopDemo/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  ShopDemo
+//
+//  Created by Neil Ajodah on 8/8/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome to Shop Demo")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
